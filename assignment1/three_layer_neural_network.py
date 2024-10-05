df34f942cf065072f32c23a339cea7867ed912fe
Cytuce <zhou923181020@gmail.com>
2024-10-05 10:09:26 -0500

    Added code skeleton for hw1 part-2

--- assignment1/three_layer_neural_network.py
+++ assignment1/three_layer_neural_network.py
@@ -27,7 +27,7 @@ def plot_decision_boundary(pred_func, X, y):
     # Generate a grid of points with distance h between them
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     # Predict the function value for the whole gid
-    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()]
     Z = Z.reshape(xx.shape)
     # Plot the contour and training examples
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
